703ff7d512ffa0c3b0b75155a7ee05605d47a185
Eduardo Rodrigues <eduardofutrodrigues@hotmail.com>
2022-08-08 17:41:05 -0300

    4 commit

---
+++ lib/grafico.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Grafico extends StatefulWidget {
+  const Grafico({key});
+
+  @override
+  State<Grafico> createState() => grafico();
+}
+
+class grafico extends State<Grafico> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('chegou');
+  }
+}
---
+++ lib/navbar.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/carteira.dart';
+import 'package:myapp/grafico.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+      initialRoute: "/",
+      routes: {
+        '/grafico': (context) => Grafico(),
+      },
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    MyHomePage2(),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color.fromARGB(255, 236, 116, 116),
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
